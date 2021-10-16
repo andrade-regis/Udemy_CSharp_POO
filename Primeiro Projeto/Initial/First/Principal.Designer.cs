22bb7a280c03a66a0b7f5e63ec9b4cde8b917e6a
Regis Andrade <regis.batista.andrade@outlook.com>
2021-10-15 21:27:39 -0300

    Inclusão_First_Project

---
+++ Primeiro Projeto/Initial/First/Principal.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace First
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExample = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExample
+            // 
+            this.btnExample.Location = new System.Drawing.Point(241, 148);
+            this.btnExample.Name = "btnExample";
+            this.btnExample.Size = new System.Drawing.Size(229, 118);
+            this.btnExample.TabIndex = 0;
+            this.btnExample.Text = "Example";
+            this.btnExample.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExample);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExample;
+    }
+}
+
---
+++ Primeiro Projeto/Initial/First/Principal.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace First
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
